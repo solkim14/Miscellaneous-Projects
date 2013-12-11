507b3bed496b6a718633dda1fbdfde48d45d9829
Sol Kim <solkim@smith.edu>
2013-12-10 21:31:23 -0500

    Project Update: 	Fades are good 	All backgrounds and images have been created 	Sound is triggering properly 	Slider is functional
    To do:
    	Title card sequence isn't working yet
    	Third sound mode has not been created
    	Sound looping is in the works
    	Not all event triggers are created (only rain right now)
    	Possible state change
    	Mandy's color change is still in the works too
    	Raheem is pretty much useless at this point. Delete at a later time.

--- Mandy/src/edu/smith/csc/csc260/Mandy/MandyApplet.java
+++ Mandy/src/edu/smith/csc/csc260/Mandy/MandyApplet.java
@@ -9,14 +9,24 @@ package edu.smith.csc.csc260.Mandy; //REMOVE FOR PROCESSING
 import processing.core.PApplet; //REMOVE FOR PROCESSING
 import processing.core.PImage;
 import controlP5.*;
-import ddf.minim.AudioPlayer;
 import ddf.minim.AudioSnippet;
 import ddf.minim.Minim;
+import gifAnimation.*;
+
+/**TO DEBUG:
+ * 
+ * fix sound loops 
+ *
+ *
+ */
 
 public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	private static final long serialVersionUID = 1L; //REMOVE FOR PROCESSING
 
 	/** FIELDS */
+	private boolean displayTitleCard = true;
+	private Gif titleCard;
+	
 	private Mandy mandy;
 	private float mandyXPos;
 	private float mandyYPos;
@@ -41,8 +51,15 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	
 	//audio player
 	Minim minim;
-	String[] songs1;
-	AudioSnippet[] songs2;	
+	AudioSnippet mode0Music;
+	boolean mode0MusicPlay = false;
+	AudioSnippet mode1Music;
+	boolean mode1MusicPlay = false;
+	AudioSnippet mode1Rain;
+	boolean mode1RainPlay = false;
+	AudioSnippet mode2Music;
+	boolean mode2MusicPlay = false;
+
 	
 	//mode 1 - rain
 	private boolean rain = false;
@@ -59,6 +76,10 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	public void setup() {
 	    size(1024,768);
 	    smooth();
+	    
+	    //title card
+		titleCard = new Gif(this, "titlecard-border.gif");
+		titleCard.loop();
 	    
 	    //setup mandy
 	    mandyXPos = width/2;
@@ -84,19 +105,16 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	    transparency = 0;
 	    img1Background = loadImage("mode1-background.png");
 	    img1Window = loadImage("mode1-window.png");
-	    img2 = loadImage("calm.jpg");
+	    img2 = loadImage("mode2-background.jpg");
 	    img3 = loadImage("yellow.jpeg");
 	    
 	    //audio setup
-		minim = new Minim(this);
-		songs1 = new String[6];
-		songs2 = new AudioSnippet[6];	
-		
-		//songs1[0]="move1.wav"; //will be static noise
-		songs1[1]="move2.wav";
-		songs1[2]="move3.wav";
+	    minim = new Minim(this);
+	    mode0Music = minim.loadSnippet("mode0-static.wav");
+	    mode1Music = minim.loadSnippet("mode1-music.mp3");
+	    mode1Rain = minim.loadSnippet("mode1-rain.wav");
+	    mode2Music = minim.loadSnippet("mode2-music.mp3");
 
-		//player.loop();
 	    
 	    //rain
 	    speed = new int[150];
@@ -111,9 +129,29 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	public void draw() {
 		background(0);
 		//raheem.render(this);
-	    
+		
+		//mandy title card
+		if (displayTitleCard == true) {
+			if (raheem.getValuePosition() != 103.5) {
+				displayTitleCard = false;
+				titleCard.stop();
+			}
+			image(titleCard,140,-15);
+		}
+		
 	    if ((raheem.getValue() >= 87) && (raheem.getValue() <= 90) && (transparency >= 0) && (transparency <= 255)) {
 	    	mode = 1;
+	    	//turn off static
+	        if (mode0MusicPlay == true) {
+	            mode0MusicPlay = false; 
+	            mode0Music.pause();
+	         }
+	        
+	        if (mode1MusicPlay == false) {
+	            mode1MusicPlay = true; 
+	            //mode1Music.loop();
+	            mode1Music.play();
+	        }
 	    	
 	    	//mandyXPos moves to 750
 	    	if (mandy.getX() < 750) {
@@ -142,11 +180,24 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	            if (key == ' ') {
 	            	if (rain == true) {
 	            		rain = false;
+	        	        mode1Rain.pause();
+	        	        
 	            	} else {
 	            		rain = true;
+	            		mode1Rain.loop(); //fix loop stuff
+	            		if (mode1Rain.position() == mode1Rain.length()) {
+		            		System.out.println("IT'S EQUAL!!!");
+	            			mode1Rain.close();	
+	            			//mode1Rain.rewind();	
+		            		//mode1Rain.play();	
+	            		}
+	            		mode1Rain.play();
 	            	}
 	            }
 	        }
+	        
+	        System.out.println("Pos: " + mode1Rain.position());
+	        System.out.println("Len: " + mode1Rain.length());
 	        
 	        if (rain == true) {
 	        	  for( int i = 0; i<150; i++) {
@@ -156,19 +207,31 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	        		  }
 	        }
 	        
-	        
-	        
 	        image(img1Window, 0, 0);
 	        if (transparency < 255) {
 	        	transparency = transparency + 5;
 	        }
 	    } else if ((raheem.getValue() >= 95) && (raheem.getValue() <= 100) && (transparency >= 0) && (transparency <= 255)) {
 	    	mode = 2;
-	    	//mandyXPos moves to 450
-	    	if (mandy.getX() < 450) {
+	    	//mandyXPos moves to 440
+	    	
+	    	//turn off static
+	        if (mode0MusicPlay == true) {
+	            mode0MusicPlay = false; 
+	            mode0Music.pause();
+	         }
+	    	
+	    	
+	        if (mode2MusicPlay == false) {
+	        	mode2MusicPlay = true; 
+	        	//mode2Music.loop();
+	        	mode2Music.play();
+	        }
+	        
+	    	if (mandy.getX() < 440) {
 	    		mandyXPos = mandyXPos+2;
 	    		mandy.setX(mandyXPos);
-	    	} else if (mandy.getX() > 450) {
+	    	} else if (mandy.getX() > 440) {
 	    		mandyXPos = mandyXPos-2;
 	    		mandy.setX(mandyXPos);
 	    	} 
@@ -186,15 +249,27 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	    	if (transparency < 255) {
 	    		transparency = transparency + 5;
 	    	}
-	    } else if ((raheem.getValue() >= 105) && (raheem.getValue() <= 108) && (transparency >= 0) && (transparency <= 255)) {
+	    } 
+	    
+	    /**
+	    else if ((raheem.getValue() >= 105) && (raheem.getValue() <= 108) && (transparency >= 0) && (transparency <= 255)) {
 	    	mode = 3;
+	    	
+	    	//turn off static
+	        if (mode0MusicPlay == true) {
+	            mode0MusicPlay = false; 
+	            mode0Music.pause();
+	         }
 	    	//
 	    	tint(255,transparency);
 	    	image(img3, 0, 0);
 	    	if (transparency < 255) {
 	    		transparency += 5; //NOTE: change all to +=
 	    	}
-	    } else {	
+	    } 
+	    */
+	    
+	    else {	
 	    	//may or may not scrap moving back to default?
 	    	/**
 	    	//mandyXPos moves to width/2
@@ -215,13 +290,33 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	    		mandy.setY(mandyYPos);
 	    	} 
 	    	*/
+	        if (mode0MusicPlay == false) {
+	            mode0MusicPlay = true; 
+	            //mode1Music.loop();
+	            mode0Music.play();
+	        }
 	    	
 	    	tint(255,transparency);
 	    	if (mode == 1) {
 		        image(img1Background, 0, 0);
 		        image(img1Window, 0, 0);
+		        
+		        if (mode1MusicPlay == true) {
+		            mode1MusicPlay = false; 
+		            mode1Music.pause();
+		         }
+		        
+            	if (rain == true) {
+            		rain = false;
+        	        mode1Rain.pause();   
+            	}
 		    } else if (mode == 2) {
 	    		image(img2, 0, 0);
+	    		
+		        if (mode2MusicPlay == true) {
+		        	mode2MusicPlay = false; 
+		            mode2Music.pause();
+		         }
 	    	} if (mode == 3) {
 	    		image(img3, 0, 0);
 	    	}
@@ -233,6 +328,7 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	    //mandy.setTint(mode);
         //image(radioTic,0,0); //figure out later
 	    mandy.render(this);
+	    
 	}
 	
 	public void mousePressed() {
@@ -251,5 +347,12 @@ public class MandyApplet extends PApplet { //REMOVE FOR PROCESSING
 	public boolean sketchFullScreen() {
 		return true;
 	}
+	
+	 public void stop() {
+	 //player.close();
+	 mode1Music.close();
+	 minim.stop();
+	 super.stop();
+	 } 
 }
 
